261418bd207ac6445fb6842010a7d067472f5775
Juan Piñeros Liberato <74014859+juandapl@users.noreply.github.com>
2023-04-27 18:00:45 +0400

    finished i think

---
+++ stress_test.c
@@ -0,0 +1,16 @@
+// stress_test.c: a stress test for the program
+// by Nicholas Raffone and Juan Piñeros
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+int main()
+{
+    pid_t pids[100];
+    
+}
